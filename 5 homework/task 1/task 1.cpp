87beba714ace542be44d03e960e46228940a1e7a
olya <klimanova.ov@phystech.edu>
2021-03-16 10:32:36 +0300

    task 1

---
+++ 5 homework/task 1/task 1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <locale>
+#include <stdexcept>
+#include <iomanip>
+
+int main()
+{
+    try
+    {
+        // Setting locale 
+#ifdef _MSC_VER
+        std::cout.imbue(std::locale("de-ch.1252"));
+#else
+        std::cout.imbue(std::locale("ru_RU.UTF-8"));
+#endif
+
+        std::cin.imbue(std::locale("en_US.UTF-8"));
+
+        // Variables
+        long double money;
+
+        // Actions
+
+        std::cout << "Money that I need to by a helicopter: ";
+        std::cin >> std::get_money(money);
+
+        std::cout << std::showbase << std::put_money(money, false) << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
+}
