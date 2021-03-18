2be5afdc2d2bee43396795163be2b0076021f062
olya <klimanova.ov@phystech.edu>
2021-03-18 21:57:55 +0300

    t2

---
+++ 6 homework/task 2/task 2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+
+int main(int argc, const char* argv[])
+{
+    //  Open file
+    std::fstream file("test.txt", std::ios::in | std::ios::out | std::ios::trunc);
+
+    //  Fill file
+    int length = 7;
+
+    std::default_random_engine gen(42);
+    std::uniform_int_distribution< int > distr(97, 122);
+
+    for (int i = 0; i < 20; ++i)
+    {
+        for (int j = 0; j < length; ++j)
+            file << static_cast<char> (distr(gen));
+        file << std::endl;
+    }
+
+    //  Find a line
+    int line;
+    std::string str;
+
+    std::cout << "Choose the line: ";
+    std::cin >> line;
+
+    file.seekg(0);
+    std::getline(file, str);
+
+    file.seekg((line - 1) * (str.size() + 1), std::ios::beg);
+
+    std::getline(file, str);
+
+    std::cout << str << std::endl;
+
+    return 0;
+}
